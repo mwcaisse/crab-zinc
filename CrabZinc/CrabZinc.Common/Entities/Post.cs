9acddec91b6b66f0e0dfb0adccca30c1c0bfbb7d
Mitchell Caisse <computers1055@gmail.com>
2018-11-06 20:57:29 -0500

    Project restructuring. Added OwlTin

--- CrabZinc/CrabZinc.Common/Entities/Post.cs
+++ CrabZinc/CrabZinc.Common/Entities/Post.cs
@@ -1,20 +1,25 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Text;
-using Mitchell.Common.Entities;
+using OwlTin.Common.Entities;
 
 namespace CrabZinc.Common.Entities
 {
-    public class BlogPost : ITrackedEntity
+    public class Post : ITrackedEntity, IActiveEntity
     {
+        public long PostId { get; set; }
 
-        public String Title { get; set; }
+        public Guid PostUuid { get; set; }
 
-        public String Content { get; set; }
+        public string Title { get; set; }
+
+        public string Content { get; set; }
 
         public DateTime PublishedDate { get; set; }
 
         public bool Visible { get; set; }
+
+        public bool Active { get; set; }
 
         public DateTime CreateDate { get; set; }
 
