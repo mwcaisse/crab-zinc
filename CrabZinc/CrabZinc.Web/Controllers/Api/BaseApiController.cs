0a1075d5457481b8d6262ce6807d07c1841f9b7b
Mitchell Caisse <computers1055@gmail.com>
2019-01-19 01:25:29 -0500

    Added Post API Controler

---
+++ CrabZinc/CrabZinc.Web/Controllers/Api/BaseApiController.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrabZinc.Web.Controllers.Api
+{
+    public class BaseApiController : Controller
+    {
+
+        protected IActionResult Get(object entity)
+        {
+            if (null == entity)
+            {
+                return NotFound();
+            }
+            return Ok(entity);
+        }
+        
+    }
+}
---
+++ CrabZinc/CrabZinc.Web/Controllers/Api/PostApiController.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CrabZinc.Common.ViewModels;
+using CrabZinc.Logic.Services;
+using Microsoft.AspNetCore.Mvc;
+
+namespace CrabZinc.Web.Controllers.Api
+{
+    [Produces("application/json")]
+    [Route("api/post/")]
+    public class PostApiController : BaseApiController
+    {
+
+        private readonly PostService _postService;
+
+        public PostApiController(PostService postService)
+        {
+            this._postService = postService;
+        }
+
+        [HttpGet]
+        [Route("{id:long}")]
+        public IActionResult Get(long id)
+        {
+            return Get(_postService.Get(id));
+        }
+
+        [HttpGet]
+        [Route("")]
+        public IActionResult GetAll()
+        {
+            return Ok(_postService.GetAll());
+        }
+
+        [HttpPost]
+        [Route("")]
+        public IActionResult Create([FromBody] PostViewModel post)
+        {
+            return Ok(_postService.Create(post));
+        }
+
+        [HttpPut]
+        [Route("{id:long}")]
+        public IActionResult Update([FromBody] PostViewModel post)
+        {
+            return Ok(_postService.Update(post));
+        }
+
+        [HttpDelete]
+        [Route("{id:long}")]
+        public IActionResult Delete(long id)
+        {
+            var post = _postService.Get(id);
+
+            if (null == post)
+            {
+                return NotFound();
+            }
+
+            return Ok(_postService.Delete(id));
+        }
+
+    }
+}
