4f6872642af4e59e27b47fd22161637c5453ccf9
Mitchell Caisse <computers1055@gmail.com>
2019-08-18 18:45:34 -0400

    Post View Controller inherits from proper controller now, to make rootpath prefix work

--- CrabZinc/CrabZinc.Web/Controllers/View/PostViewController.cs
+++ CrabZinc/CrabZinc.Web/Controllers/View/PostViewController.cs
@@ -4,19 +4,21 @@ using System.Linq;
 using System.Threading.Tasks;
 using CrabZinc.Common.ViewModels;
 using CrabZinc.Logic.Services;
+using CrabZinc.Web.Configuration;
 using CrabZinc.Web.Util;
 using Microsoft.AspNetCore.Mvc;
 
 namespace CrabZinc.Web.Controllers.View
 {
     [Route("post/")]
-    public class PostViewController : Controller
+    public class PostViewController : BaseViewController
     {
 
         private readonly PostService _postService;
         private readonly MarkdownRenderer _markdownRenderer;
 
-        public PostViewController(PostService postService, MarkdownRenderer markdownRenderer)
+        public PostViewController(ApplicationConfiguration applicationConfiguration, PostService postService, 
+            MarkdownRenderer markdownRenderer) : base(applicationConfiguration)
         {
             this._postService = postService;
             this._markdownRenderer = markdownRenderer;
